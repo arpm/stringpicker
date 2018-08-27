5ad5fc59e2fb0e294027f99c0362e81b34f90c94
Andres Pena <arpm96@hotmail.com>
2018-08-27 14:03:20 -0500

    Improve the digits formatting and rename some variables

--- stringpicker.py
+++ stringpicker.py
@@ -7,6 +7,9 @@ per line desired"""
 import random
 import sys
 
+DIGITS_FORMAT = {1: "{:0>1} ", 2: "{:0>2} ", 3: "{:0>3} ", 4: "{:0>4} ",
+                 5: "{:0>5} ", 6: "{:0>6} ", 7: "{:0>7} ", 8: "{:0>8} "}
+
 
 def file_to_list(inputFile):
     """Returns the input file as a list"""
@@ -21,13 +24,14 @@ def longest_string_length(inputList):
     return len(max(inputList, key=len))
 
 
-def string_picker(inputFile, numberOfPicks, stringsPerLine):
+def string_picker(inputFile, numberOfStrings, stringsPerLine):
     """Returns the randomly selected strings as a formatted string"""
 
     inputList = file_to_list(inputFile)
 
-    selectedStrings = random.sample(inputList, numberOfPicks)
+    selectedStrings = random.sample(inputList, numberOfStrings)
     longestStringLength = longest_string_length(selectedStrings)
+    numberOfDigits = len(str(numberOfStrings))
 
     outputString = ""
 
@@ -35,13 +39,14 @@ def string_picker(inputFile, numberOfPicks, stringsPerLine):
 
         currentStringLength = len(string)
         currentStringIndex = selectedStrings.index(string)
-        currentStringNumberFormat = "{:0>2} ".format(currentStringIndex + 1)
+        currentStringNumber = DIGITS_FORMAT[numberOfDigits].format(
+                              currentStringIndex + 1)
         padding = (longestStringLength - currentStringLength + 2) * " "
 
         if currentStringIndex % stringsPerLine == 0:
             outputString += "\n"
 
-        outputString += currentStringNumberFormat + string + padding
+        outputString += currentStringNumber + string + padding
 
     outputString += "\n"
 
