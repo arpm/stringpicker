7668d847ade882ad05d9e12470aae5e50014a9ca
Andres Pena <arpm96@hotmail.com>
2018-08-27 13:40:42 -0500

    Refactor code for readability and functionality

--- stringpicker.py
+++ stringpicker.py
@@ -1,59 +1,49 @@
 #!/usr/bin/env python
 
-"""Picks random elements of a list given the list itself and the number
-of picks desired by the user"""
+"""Outputs a list of randomly selected strings given the name of an
+input file, the number of strings desired and the number of strings
+per line desired"""
 
 import random
 import sys
 
 
+def file_to_list(inputFile):
+    """Returns the input file as a list"""
+    file = open(inputFile, 'r')
+    strings = [line.rstrip() for line in file]
+    file.close()
+    return strings
+
+
 def longest_string_length(inputList):
+    """Returns the length of the longest string from the input list"""
     return len(max(inputList, key=len))
 
 
-def file_to_list(inputFile):
-    file = open(inputFile, 'r')
-    questions = [line.rstrip() for line in file]
-    return questions
-
-
-def string_picker(inputFile, numberOfPicks, elementsPerLine):
-    """Formats and print the list of inputs"""
+def string_picker(inputFile, numberOfPicks, stringsPerLine):
+    """Returns the randomly selected strings as a formatted string"""
 
     inputList = file_to_list(inputFile)
 
-    selected = random.sample(inputList, numberOfPicks)
-    longestStringLength = longest_string_length(selected)
+    selectedStrings = random.sample(inputList, numberOfPicks)
+    longestStringLength = longest_string_length(selectedStrings)
 
     outputString = ""
 
-    for question in selected:
+    for string in selectedStrings:
 
-        currentStringLength = len(question)
-        currentStringIndex = selected.index(question)
+        currentStringLength = len(string)
+        currentStringIndex = selectedStrings.index(string)
         currentStringNumberFormat = "{:0>2} ".format(currentStringIndex + 1)
         padding = (longestStringLength - currentStringLength + 2) * " "
 
-        if currentStringIndex % elementsPerLine == 0:
+        if currentStringIndex % stringsPerLine == 0:
             outputString += "\n"
 
-        outputString += currentStringNumberFormat + question + padding
+        outputString += currentStringNumberFormat + string + padding
 
     outputString += "\n"
-
-    # for index in range(0, len(selected)):
-
-    #     current_string_length = len(selected[index])
-
-    #     # this is the number of spaces required to make the output readable
-    #     spaces =  longest_string_length - current_string_length + PADDING
-
-    #     # splits every line of output given the 'elementsPerLine'
-    #     output_string += "\n" if index % elementsPerLine == 0 else ""
-
-    #     # this is the format that is going to be used for the output
-    #     output_string += ("{:0>2d}".format(index + 1) + " "
-    #                       + selected[index] + " " * spaces)
 
     return outputString
 
